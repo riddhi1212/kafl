84f56f48400eea04122ada4efe11a74f9f1ef86d
Riddhi Mittal <riddhi@Riddhis-MacBook-Pro.local>
2014-09-14 15:00:27 +0530

    basic form changes and Post functionality via jquery

--- js/app.js
+++ js/app.js
@@ -1,14 +1,32 @@
 var main = function() {
 	console.log("in main");
 	$('#found-post-btn').click(function(){
-		console.log('btn clicked');
 		var name = $('#found-name').val();
-		$('<li>').text(name).addClass('list-group-item').prependTo('.found-people-list');
+
+		if (name.length === 0) return;
+
+		var age = $('#found-age').val();
+		var by = $('#found-by').val();
+		var div = $('<div>').addClass('row');
+		$('<p>').addClass('col-md-4').text(name).appendTo(div);
+		$('<p>').addClass('col-md-4').text(age).appendTo(div);
+		$('<p>').addClass('col-md-4').text(by).appendTo(div);
+		$('<li>').append(div).addClass('list-group-item').prependTo('.found-people-list'); 		
+
+//		$('<li>').text(name).addClass('list-group-item').prependTo('.found-people-list');
+		$('#found-name').val('');
+		$('#found-age').val('');
+		$('#found-tel').val('');
+		$('#found-by').val('');
 	});
 	$('#find-post-btn').click(function(){
-		console.log('btn clicked');
 		var name = $('#find-name').val();
+		if (name.length === 0) return;
 		$('<li>').text(name).addClass('list-group-item').prependTo('.find-people-list');
+
+		$('#find-name').val('');
+		$('#find-age').val('');
+		$('#find-tel').val('');
 	});
 }
 
